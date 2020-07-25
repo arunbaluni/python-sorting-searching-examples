25b1b69502df6359718354ccdc0011417388eaf9
arunbaluni <arunbaluni@gmail.com>
2020-07-25 20:42:30 +0530

    Bubble Sort example

---
+++ bubble-sort.py
@@ -0,0 +1,28 @@
+# Bubble Sorting
+
+def bubble_sort(input_list):
+	pass_value = 0
+	for x in range(len(input_list) - 1, 0, -1):
+		for y in range(x):
+			if(input_list[y] > input_list[y + 1]):
+				temp = input_list[y + 1]
+				input_list[y + 1] = input_list[y]
+				input_list[y] = temp
+			else:
+				continue
+		pass_value += 1
+		print("Current sorted list in pass {} is {}: ".format(pass_value, input_list))
+
+
+def main():
+	input_list = [91, 45, 23, 59, 0, 3,-11, 65]
+	print("##########################\n")
+	print("Input list to be sorted:")
+	print(input_list, "\n")
+	bubble_sort(input_list)
+	print("\nFinal sorted list:", input_list)
+	print("##########################")
+
+
+if(__name__=="__main__"):
+	main()
